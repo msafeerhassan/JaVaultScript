09da530766d83ed94f68b4b8f8fbfc9cab0652e4
msafeerhassan <msafeerhassan0@gmail.com>
2026-06-08 15:40:36 +0500

    working here too - functionalities added

--- app.js
+++ app.js
@@ -99,4 +99,84 @@ lockBtn.addEventListener("click", () => {
 
     mainApp.classList.add("hidden");
     splashWall.classList.remove("hidden")
-})
+});
+
+let peerConnection = null;
+let dataChannel = null;
+
+const genOfferBtn = document.getElementById("genOfferBtn");
+const localSdpTextArea = document.getElementById("localSdpTextArea");
+const remoteSdpTextArea = document.getElementById("remoteSdpTextArea");
+const acceptRemoteBtn = document.getElementById("acceptRemoteBtn")
+
+function initializePeerConnection() {
+    console.log("Initializing Peer Connection!");
+
+    const configuration = {
+        iceServers: [
+        {
+            urls: "stun:stun.l.google.com:19302"
+        }
+        ]
+    };
+
+    peerConnection = new RTCPeerConnection(configuration);
+
+    dataChannel = peerConnection.createDataChannel("chatChannel");
+    setupDataChannelListeners(dataChannel);
+
+    peerConnection.onconnectionstatechange = () => {
+        console.log(`WebRTC Connection State Updated: ${peerConnection.connectionState}`);
+    };
+    peerConnection.oniceconnectionstatechange = () => {
+        console.log(`WebRTC ICE Connection Status Update: ${peerConnection.iceConnectionState}`);
+    };
+
+    peerConnection.ondatachannel = (event) => {
+        console.log("Inbound remote WebRTC data Channel Captured");
+        setupDataChannelListeners(event.channel)
+    };
+}
+
+
+function setupDataChannelListeners(channel) {
+    channel.onopen = () => {
+        console.log("Data Channel Link Stauts: Open");
+    };
+    channel.onclose = () => {
+        console.log("Data Channel Link Status: Close");
+    }
+    channel.onerror = (error) => {
+        console.error("Data Channel Communication Fault", error);
+    }
+    channel.onmessage = (event) => {
+        console.log("Encrypted Data Recieved over Data Channel");
+
+    };
+}
+
+async function generateLocalConnectionOffer(p) {
+    if(!peerConnection) {
+        initializePeerConnection();
+    }
+    try {
+        const offer = await peerConnection.createOffer();
+        await peerConnection.setLocalDescription(offer);
+
+        peerConnection.onicecandidate = (event) => {
+            if(!event.candidate) {
+                console.log("ICE Network Parsing done :)");
+                localSdpTextArea.value = btoa(JSON.stringify(peerConnection.localDescription));
+            }
+        };
+
+        if (peerConnection.iceGatheringState === "complete") {
+            localSdpTextArea.value = btoa(JSON.stringify(peerConnection.localDescription));
+        }
+    } catch (error) {
+        console.error("Failed to construct local connection configuration profile :(");
+        alert("Strucutre Initialization Failure.");
+    }
+}
+
+genOfferBtn.addEventListener("click", generateLocalConnectionOffer);
